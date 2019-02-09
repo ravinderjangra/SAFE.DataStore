16c6d677fb8db8dde2de7692dccc84a6f114a454
oetyng <oetyng@gmail.com>
2019-02-09 14:51:43 +0100

    websockets instead of named pipes for interprocess comm (since netstandard 2.0 does not handle it yet)

--- SAFE.DataStore/Client/ClientFactory.cs
+++ SAFE.DataStore/Client/ClientFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
 using SAFE.DataStore.Client.Auth;
 
 namespace SAFE.DataStore.Client
@@ -30,14 +32,20 @@ namespace SAFE.DataStore.Client
             // Authentication with the SAFE browser
             await _authentication.AuthenticationWithBrowserAsync();
 
-            // Start named pipe server and listen for message
-            var authResponse = App.PipeComm.ReceiveNamedPipeServerMessage();
+            // NamedPipes ACL not working in netcore, see https://github.com/dotnet/corefx/issues/31190
 
-            // Create session from response
-            var session = await _authentication.ProcessAuthenticationResponse(authResponse);
-            return new SAFEClient(session, _appId);
+            // Start websocket server and listen for message
+            using (var server = new ResponseSocket("@tcp://localhost:5556"))
+            {
+                var authResponse = server.ReceiveFrameString();
+                server.SendFrame("Auth response received.");
+
+                // Create session from response
+                var session = await _authentication.ProcessAuthenticationResponse(authResponse);
+                return new SAFEClient(session, _appId);
+            }
         }
 
-        // GetAlpha-2Client
+        // Get Alpha-2 / Fleming Client
     }
 }
